70f8b59368bd9429732fc4aec9cb88d2e5d0352b
Piicksarn <zsghsn@gmail.com>
2016-03-29 11:26:02 +0800

    Showing the data of setting pad.

---
+++ js/DataShow.js
@@ -0,0 +1,17 @@
+var Data = function() {
+  this.itemNameList = new Array();
+  this.valueList = new Array();
+};
+Data.prototype = {
+  setItem: function() {
+
+  },
+  setValue: function() {
+
+  },
+  show: function(divId) {
+
+
+
+  }
+}
